d555a09d59e07c396b348d55b0c95964f861a4cf
kanav <kanavrana91@gmail.com>
2024-10-26 22:53:42 +0530

    add the bycrpt for hidding the password in database

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 mongoose.connect("mongodb://localhost:27017/tsuki").then(() => {
   console.log("Mongodb is connected");
 });
@@ -8,6 +9,14 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
+});
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("user", userSchema);
